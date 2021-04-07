0defd79ea7f28b514351c136ef2272c7aefe7154
Adrian Kato <adriankato@gmail.com>
2021-04-06 22:08:52 -0400

    minor error change url name

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
-  assetPrefix: ghPages ? '/dotcs.github.io/' : ''   // customize this value
+  assetPrefix: ghPages ? '/adriankato.github.io/' : ''   // customize this value
 };
